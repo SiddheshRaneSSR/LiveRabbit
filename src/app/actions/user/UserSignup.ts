fb586218a96b7729a000a04569b6eb2ff97b8a17
Siddhesh <Siddharthrane123.sr@gmail.com>
2024-12-05 15:07:08 +0530

    added server actions

---
+++ src/app/actions/user/UserSignup.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+
+export async  function post(req:NextRequest){
+
+    const body = await req.json();
+    const email = body.email;
+    const password = body.password;
+
+    
+
+
+}
